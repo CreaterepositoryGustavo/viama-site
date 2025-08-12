adf4331058961b179e9fbcbddd47b56b12854ff6
CreaterepositoryGustavo <gapessino@gmail.com>
2025-08-12 13:05:57 -0300

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,3 @@
 import '../styles/globals.css';
 export const metadata={title:'Viama Viajes y Turismo',description:'Te ayudamos a vivir experiencias'};
 export default function RootLayout({children}){return(<html lang="es"><body>{children}</body></html>);}
-fix: ruta de globals.css
